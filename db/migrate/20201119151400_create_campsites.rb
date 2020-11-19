09201e2d29ab87052c68a8a2c2253fde7eda30d7
sotahasimoto <sotahashimoto316@gmail.com>
2020-11-20 00:20:36 +0900

    Model追加

---
+++ db/migrate/20201119151400_create_campsites.rb
@@ -0,0 +1,12 @@
+class CreateCampsites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :campsites do |t|
+      t.string :name
+      t.string :postcode
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
+  end
+end
