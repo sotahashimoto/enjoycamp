e7b02ceec29fbf2f8e25036d47137ea72ec4ca6f
sotahasimoto <sotahashimoto316@gmail.com>
2020-11-28 15:50:00 +0900

    CircleCI追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :campsite
 
-  # with_options presence: true do
-    # validates :content
+  with_options presence: true do
+    validates :content
     # validates :score
-  # end
+  end
 end
