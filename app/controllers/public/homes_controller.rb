938b14840647e8cf13df37f2a57da1469779440a
sotahasimoto <sotahashimoto316@gmail.com>
2020-12-01 00:45:18 +0900

    ゲストログイン終了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,6 +4,7 @@ class Public::HomesController < ApplicationController
   def top
   end
 
+  # ゲストログイン機能
   def new_guest
     user = User.find_or_create_by!(name: 'ゲスト太郎', favorite_place: "舞洲バーベキューパーク", introduction: "年齢:平成6年男子, キャンプ歴:2019年10月～, 拠点:山陰, テント:サーカスtc、名も無きスクリーンタープ", email: 'guesttest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
@@ -12,6 +13,8 @@ class Public::HomesController < ApplicationController
       redirect_to public_campsites_top_path
   end
 
+  private
+  # ログインしている時homes#topには飛べない
   def redirect_root
     redirect_to public_campsites_top_path if user_signed_in?
   end
